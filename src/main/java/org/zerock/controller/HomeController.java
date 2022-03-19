bd9f42fcdeae10e4c70e15ae9ab9ec2793e943b9
jiseong0612 <hth5545@gmail.com>
2022-03-19 18:21:14 +0900

    파라미터 에러 @controllerAdvice, @ExceptionHandler() 사용

--- src/main/java/org/zerock/controller/HomeController.java
+++ src/main/java/org/zerock/controller/HomeController.java
@@ -8,8 +8,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.zerock.sample.SampleDto;
 
 /**
  * Handles requests for the application home page.
@@ -36,4 +39,15 @@ public class HomeController {
 		return "home";
 	}
 	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+	
+	@PostMapping("/hellopost")
+	public String hellopost(SampleDto dto) {
+		System.out.println(dto.getName());
+		System.out.println(dto.getAge());
+		return "redirect:/hello";
+	}
 }
---
+++ src/main/java/org/zerock/sample/SampleDto.java
@@ -0,0 +1,9 @@
+package org.zerock.sample;
+
+import lombok.Data;
+
+@Data
+public class SampleDto {
+	private String name;
+	private int age;
+}

852f85659ee478196c0294dc1510224c18aa3a42
jiseong0612 <hth5545@gmail.com>
2023-10-14 04:01:52 +0900

    @data 추가, list 아직 존재지 않는 파라미터 jsp 에러 -> 주석처리

--- src/main/java/org/zerock/domain/PageDTO.java
+++ src/main/java/org/zerock/domain/PageDTO.java
@@ -1,26 +1,29 @@
 package org.zerock.domain;
 
+import lombok.Data;
+
+@Data
 public class PageDTO {
 
 	private int total, startPage, endPage;
 	private boolean prev, next;
 	private Criteria cri;
-	
+
 	public PageDTO(Criteria cri, int total) {
 		this.cri = cri;
 		this.total = total;
-		
+
 		this.endPage = (int) Math.ceil(cri.getPageNum() / 10.0) * 10;
-		this.startPage= endPage - 9;
-		
+		this.startPage = endPage - 9;
+
 		int realEnd = (int) Math.ceil(total * 1.0 / cri.getAmount());
-		
-		if(realEnd < this.endPage) {
-			this.endPage = realEnd; 
+
+		if (realEnd < this.endPage) {
+			this.endPage = realEnd;
 		}
-		
+
 		this.prev = this.startPage > 1;
 		this.next = this.endPage < realEnd;
-		
+
 	}
 }

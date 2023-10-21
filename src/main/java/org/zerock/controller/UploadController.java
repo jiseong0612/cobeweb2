f5a5650ed7c2f5e9164ce4ec99c506a30732b2f1
jiseong0612 <hth5545@gmail.com>
2023-10-22 01:56:46 +0900

    AttachFileDTO 생성, uploadAjax.jsp에 비디오 태그 추가, 동영상 재생 test

--- src/main/java/org/zerock/controller/UploadController.java
+++ src/main/java/org/zerock/controller/UploadController.java
@@ -5,24 +5,28 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.domain.AttachFileDTO;
 
 import lombok.extern.slf4j.Slf4j;
 import net.coobird.thumbnailator.Thumbnailator;
-import oracle.security.crypto.core.AlgID;
 
 @Controller
 @Slf4j
@@ -38,19 +42,20 @@ public class UploadController {
 		return "uploadAjax";
 	}
 	
-	
 	@PostMapping("/uploadFormAction")
-	public void uploadFormAction(MultipartFile[] uploadFile, @RequestParam Map<String, String> inMap,HttpServletResponse response) throws IllegalStateException, IOException {
-		String uploadFoler = "C:\\Users\\hjs\\Desktop\\test";
+	public ResponseEntity<List<AttachFileDTO>> uploadFormAction(MultipartFile[] uploadFile, @RequestParam Map<String, String> inMap,HttpServletResponse response) throws IllegalStateException, IOException {
+		List<AttachFileDTO> list = new ArrayList<AttachFileDTO>();
+		String uploadFolder = "C:\\Users\\hjs\\Desktop\\test";
+		String uploadFolderPath = getFolder();
+		File uploadPath = new File(uploadFolder, uploadFolderPath);
 		
-		File uploadPath = new File(uploadFoler, getFolder());
-		
+		//today 년/월/일 폴더 생성
 		if(!uploadPath.exists()) {
 			uploadPath.mkdirs();
 		}
 		
-		
 		for(MultipartFile file : uploadFile) {
+			AttachFileDTO attach = new AttachFileDTO();
 			log.info("getOriginalFilename : " +file.getOriginalFilename());
 			log.info("getContentType : " +file.getContentType());
 			log.info("getName : " +file.getName());
@@ -60,16 +65,43 @@ public class UploadController {
 			File saveFile = new File(uploadPath, uploadFileName);
 			file.transferTo(saveFile);
 			
+			//attach 객체 세팅
+			attach.setFileName(file.getOriginalFilename());
+			attach.setUuid(uuid);
+			attach.setUploadPath(uploadFolderPath);
+			
+			//이미지인 경우 섬네일 생성
 			if(checkImageType(saveFile)) {
+				attach.setImage(true);
 				FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath,"s_"+uploadFileName));
 				Thumbnailator.createThumbnail(file.getInputStream(), thumbnail, 100, 100);
 				
 				thumbnail.close();
 			}
 			
+			list.add(attach);
 		}
 		
-		response.setStatus(HttpStatus.OK.value());
+		return new ResponseEntity<List<AttachFileDTO>>(list, HttpStatus.OK);
+	}
+	
+	@GetMapping("/display")
+	public ResponseEntity<byte[]> getFiles(String fileName){
+		String uploadFolder =  "C:\\Users\\hjs\\Desktop\\test";
+		File file = new File(uploadFolder, fileName);
+		
+		ResponseEntity<byte[]> result = null;
+		try {
+			HttpHeaders header = new HttpHeaders();
+			
+			header.setContentType(MediaType.parseMediaType(Files.probeContentType(file.toPath())));
+			
+			result = new ResponseEntity<byte[]>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
 	}
 	
 	private String getFolder() {
--- src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -10,11 +10,45 @@
 <body>
 <h1>upload with ajax</h1>
 <div>
+	
+	<video controls="controls" width="1000">
+		<source src="/display?fileName=2023/10/22/5d64ae0a-66c9-4c0d-91be-a8e5162abfde_SampleVideo_10mb.mp4">
+		<source src="/display?fil4">
+		
+		Download the
+		  <a href="/display?fileName=2023/10/22/5d64ae0a-66c9-4c0d-91be-a8e5162abfde_SampleVideo_10mb.mp4">WEBM</a>
+		  or
+		  <a href="/media/cc0-videos/flower.mp4">MP4</a>
+ 	 video.
+	</video>
+	<br>
+	<br>
+	<br>
 	<input type="file" name="uploadFile"  multiple="multiple">
+	<br>
 	
 	<button>submit</button>
 </div>
+<div class="uploadDiv">
+	<ul></ul>
+</div>
 <script>
+var uploadResult = $('.uploadDiv ul');
+
+var showUploadedFile = function(uploadResultArr){
+	var str = '';
+	
+	$(uploadResultArr).each(function(i, obj){
+		if(!obj.image){
+			str += '<li><img src="/resources/img/attachment.png"></li>';
+		}else{
+			str += '<li><img src="/display?fileName='+obj.uploadPath+'/s_'+obj.uuid+'_'+obj.fileName+'">'+obj.fileName+'</li>';
+		}
+	});
+	
+	uploadResult.append(str);
+};
+
 	$(document).ready(function(){
 		$('button').on('click', function(){
 			var files = $('[name=uploadFile]')[0].files;
@@ -31,11 +65,13 @@
 				data : formData,
 				contentType : false,
 				processData : false,
+				dataType : 'json',
 				success : function(result){
-					alert("upload");
+					$('[type=file]').val('');
+					
+					showUploadedFile(result);
 				}
 			});
-			
 		});
 	});
 </script>

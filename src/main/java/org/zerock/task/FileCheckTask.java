68417a79cf2144c19029c4373ea8216955ddef6c
jiseong0612 <hth5545@gmail.com>
2023-10-25 17:27:34 +0900

    쿼츠, 스케쥴 로그 찍히는거 확인함

---
+++ src/main/java/org/zerock/task/FileCheckTask.java
@@ -0,0 +1,18 @@
+package org.zerock.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.log4j.Log4j;
+
+@Component
+@Log4j
+public class FileCheckTask {
+
+	@Scheduled(cron="2 * * * * *")
+	public void checkFiless()throws Exception{
+		log.debug("file check task run......");
+		log.debug("=====================");
+		
+	}
+}

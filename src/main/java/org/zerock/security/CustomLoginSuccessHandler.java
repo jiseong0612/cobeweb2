b6d2351eb56c2f4cced40d31bb583ed5ca9520eb
jiseong0612 <hth5545@gmail.com>
2023-10-29 21:33:59 +0900

    로그인 성공시 세션에 로그인된 객체 정보를 담을 수 있다

--- src/main/java/org/zerock/security/CustomLoginSuccessHandler.java
+++ src/main/java/org/zerock/security/CustomLoginSuccessHandler.java
@@ -9,7 +9,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.zerock.domain.MemberVO;
+import org.zerock.security.domain.CustomUser;
 
 import lombok.extern.log4j.Log4j;
 
@@ -22,6 +25,14 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler{
 		log.warn("login success...");
 		
 		List<String> roleNames = new ArrayList<String>();
+		CustomUser customUser =  (CustomUser)authentication.getPrincipal();
+		User user =  (User)authentication.getPrincipal();
+		
+		MemberVO member = customUser.getMember();
+		
+		//추가 작업으로 로그인 성공시 세션에 로그인된 객체 정보를 담을 수 있다.
+		log.info("logined member : " + member);
+		
 		
 		authentication.getAuthorities().forEach(autht ->{
 			roleNames.add(autht.getAuthority());
